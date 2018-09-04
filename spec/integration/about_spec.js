3d5530b8cd1387836e4cadfcf55bfe042cfbd1a6
clintonm3 <clmurphy789@gmail.com>
2018-09-03 19:59:32 -0600

    Assignment 4 Complete

---
+++ spec/integration/about_spec.js
@@ -0,0 +1,15 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/about";
+
+describe("routes : about", () => {
+  describe("GET /about", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+        done();
+      });
+    });
+   });
+});
--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,5 +2,7 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     app.use(staticRoutes);
+    const aboutRoute = require("../routes/about");
+    app.use(aboutRoute);
   }
 }
