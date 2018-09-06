9bb0a4d6ab8fdcdfd8c3f20e1e19c3263719d49e
clintonm3 <clmurphy789@gmail.com>
2018-09-05 20:31:27 -0600

    Assignment 6 Topic Resource Part 2: complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,9 +3,11 @@ module.exports = {
     const staticRoutes = require("../routes/static");
     const topicRoutes = require("../routes/topics");
     const aboutRoute = require("../routes/about");
+    const advertisementRoutes = require('../routes/advertisements');
 
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(aboutRoute);
+    app.use(advertisementRoutes);
   }
 }
